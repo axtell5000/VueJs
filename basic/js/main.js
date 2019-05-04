e05c50227e67a4f2a52223e20c5736d8e2f29f7b
Stephen Axtell <axtell.stephen@gmail.com>
2019-05-04 15:59:14 +0200

    working with the DOM

--- basic/js/main.js
+++ basic/js/main.js
@@ -1,12 +1,18 @@
 new Vue({
   el: '#app',
   data: {
-    title: 'Hello World',
-    link: 'http://google.com'
+    title: 'Hello World!!',
+    titleB: 'Hello World',
+    link: 'http://google.com',
+    finishedLink: '<a href="http://www.iol.co.za">IOL</a>'
   },
   methods: {
     sayHowzit: function() {
-      return this.title; // vue behind the scenes is allowing this to point to data properties
+      this.titleB = 'Hello';
+      return this.titleB; // vue behind the scenes is allowing this to point to data properties
+    },
+    changeTitle: function(event) {
+      this.title = event.target.value;
     }
   }
 });
