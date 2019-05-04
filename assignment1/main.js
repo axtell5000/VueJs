304f56e0070635b97253ff77104e537aee60454f
Stephen Axtell <axtell.stephen@gmail.com>
2019-05-04 18:47:48 +0200

    small exercise to reinforce knowledge

---
+++ assignment1/main.js
@@ -0,0 +1,13 @@
+new Vue({
+  el: '#exercise',
+  data: {
+    name: 'Stephen',
+    age: 25,
+    imgUrl: 'https://2.bp.blogspot.com/-x3tpjKhFK7E/VoMEwvPNqrI/AAAAAAABh-U/oyEXybVExEcbLEoD3EpZvU_R7JkgehZQACPcB/s1600/tessafowlerholidaylooksm.jpg'
+  },
+  methods: {
+    randomFloat: function() {
+      return Math.random();
+    }
+  }
+});
