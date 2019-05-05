b0228f6d7f6ede2fe541e51a90e4b799bef71e0d
Stephen Axtell <axtell.stephen@gmail.com>
2019-05-05 16:10:43 +0200

    working with Vue events and modifiers

--- basic/js/main.js
+++ basic/js/main.js
@@ -17,3 +17,23 @@ new Vue({
   }
 });
 
+new Vue({
+  el: '#button',
+  data: {
+    counter: 0,
+    x: 0,
+    y: 0
+  },
+  methods: {
+    increase: function(step, event) {
+      this.counter += step;
+    },
+    updateCoordinates: function(event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
+    },
+    alertMe: function () {
+      alert('Hello');
+    }
+  }
+});
