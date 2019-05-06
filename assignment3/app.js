f72dde807eb72f7894abc3c440ee0941e7e997a9
Stephen Axtell <axtell.stephen@gmail.com>
2019-05-06 12:44:38 +0200

    added some more comments

--- assignment3/app.js
+++ assignment3/app.js
@@ -4,12 +4,14 @@ new Vue({
             value: 0            
         },
         // Nice article here re meaning: - https://css-tricks.com/methods-computed-and-watchers-in-vue-js/
+        // keeps things 'cached' and only changes if data properties been used has changed - better option
         computed: {
             result: function() {
                 console.log('Computed');
     	        return this.value >= 37 ? this.result = 'done' : 'Not there yet';
             }
         },
+        // good with async operations
         watch: {
             result: function() {
             var vm = this;
