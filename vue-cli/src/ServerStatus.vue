49d6c24828c58e9f10889331375439aeaf3dca11
Stephen Axtell <axtell.stephen@gmail.com>
2019-05-11 11:43:28 +0200

    Added some Vue components and started with mdular code

---
+++ vue-cli/src/ServerStatus.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- Like React have to wrap multiple elements, Template doesnt count -->
+  <div>
+    <p>Server Status: {{ status }}</p>
+    <hr>
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => {
+      return {
+        status: 'Critical'
+      };
+    },
+    methods: {
+      changeStatus() {
+        this.status = 'Normal';
+      }
+    }
+  }
+</script>
