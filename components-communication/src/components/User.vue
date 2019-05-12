8729d5d29805508c0e464fa53a47cf10b7a2e4df
Stephen Axtell <axtell.stephen@gmail.com>
2019-05-12 18:56:51 +0200

    communicating between components parent to child  and child to parent

---
+++ components-communication/src/components/User.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my Name</button>
+        <p>Name is: {{ name }}</p>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail 
+                    v-bind:myName="name" 
+                    @nameWasReset="name = $event"
+                    :resetFn="resetName"
+                    :userAge="age"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit 
+                    :userAge="age"
+                    @ageWasEdited="age = $event"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        // In a non root component, we have to do put data in a function and return it.
+        // We do this so data can be separated and not referencing the same thing in memory
+        data: () => ({name: 'Stephen', age: 37}),
+        methods: {
+            changeName() {
+                this.name = 'Jenny';
+            },
+            resetName() {
+                this.name = 'Stephen';
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ components-communication/src/components/UserDetail.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{switchName() }}</p>
+        <p>User Age: {{ userAge }}</p>
+        <button @click="resetName">Reset Name</button>
+        <button @click="resetFn()">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        //props: ['myName'],
+        // We can use an Object for props, here we can also force the prop type. Here we must use a String.
+        // e,g props: {myName: String}
+        // Another way is like below
+        props: {
+            myName: {
+                type: String,
+                required: true,
+                default: 'Stephen'
+            },
+            resetFn: Function,
+            userAge: Number
+        },
+        methods: {
+            switchName() {
+                return this.myName.split('').reverse().join('');
+            },
+            resetName() {
+                this.myName = 'BIg Steppa';
+                this.$emit('nameWasReset', this.myName);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
