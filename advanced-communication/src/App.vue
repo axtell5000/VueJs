0a6e535a44560bbed21a7fde392a349dbea9a096
Stephen Axtell <axtell.stephen@gmail.com>
2019-05-22 18:15:42 +0200

    advanced communication between components - includes slots and dynamic content

---
+++ advanced-communication/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+                <!-- component - reserved word to hold dynamic content -->
+                <keep-alive><!-- If we dont do this the component will be destroyed when we go to other. Using this disables destroy -->
+                    <component :is="selectedComponent"><p>Default Content</p></component>
+                </keep-alive>
+               <!-- <app-quote>
+                   Correspond the slot to the name attribute in the child 
+                   <h2 slot="title">{{ quoteTitle }}</h2>
+                   <p slot="content">I am Iron Man</p>
+               </app-quote> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
+
+    export default {
+        components: {
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
+        },
+        data: function() {
+            return {
+                quoteTitle: 'The Quote',
+                selectedComponent: 'appQuote'
+            };
+        }
+    };
+</script>
+
+<style>
+</style>
---
+++ advanced-communication/src/components/Quote.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="title">
+      <slot name="title"></slot><!--This allows us to render a chunk of HTML from outside. Form parent to child -->
+      <span style="color: #ccc"><slot name="subtitle">The Subtitle</slot></span><!-- Use as default -->
+    </div>
+    <hr>
+    <div>
+      <!-- You have to use names if you want to refine your display more -->
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  destroyed() {
+    console.log('Destroyed!');
+  },
+  deactivated() {
+    console.log('Deactivated!');
+  },
+  activated() {
+    console.log('Activated!');
+  }
+}
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    margin: 30px auto;
+    padding: 30px;
+    text-align: center;
+  }
+
+  h2 {
+    color: red;
+  }
+
+  .title {
+    font-style: italic;
+  }
+</style>
