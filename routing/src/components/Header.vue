065b5164e8cae032371d4422628d2411867b69f1
Stephen Axtell <axtell.stephen@gmail.com>
2019-06-18 12:35:33 +0200

    adding a header and nav

---
+++ routing/src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <ul class="nav nav-pills">
+    <!-- Just like Angular and React, 'exact' forces the link to be exact, for example the '/' appears in both and could cause issues -->   
+    <router-link to="/" active-class="active" tag="li" exact><a>Home</a></router-link>
+    <router-link to="/user" active-class="active" tag="li"><a>User</a></router-link>
+  </ul>
+</template>
--- routing/src/main.js
+++ routing/src/main.js
@@ -1,7 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import App from './App.vue';
+import { routes } from './routes';
+
+Vue.use(VueRouter); // inserting new package into vue core
+
+// creating our new router
+const router = new VueRouter({
+  routes: routes,
+  mode: 'history' // have to do this if you want nice clean urls, not the ones with a hash
+});
 
 new Vue({
   el: '#app',
+  router: router,  
   render: h => h(App)
-})
+});
