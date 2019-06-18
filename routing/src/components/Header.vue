8e01dcfe495de5b8be40f9f5c8d39da772a06f36
Stephen Axtell <axtell.stephen@gmail.com>
2019-06-18 15:04:24 +0200

    allow using parameters in url

--- routing/src/components/Header.vue
+++ routing/src/components/Header.vue
@@ -2,6 +2,7 @@
   <ul class="nav nav-pills">
     <!-- Just like Angular and React, 'exact' forces the link to be exact, for example the '/' appears in both and could cause issues -->   
     <router-link to="/" active-class="active" tag="li" exact><a>Home</a></router-link>
-    <router-link to="/user" active-class="active" tag="li"><a>User</a></router-link>
+    <router-link to="/user/10" active-class="active" tag="li"><a>User 10</a></router-link>
+    <router-link to="/user/20" active-class="active" tag="li"><a>User 20</a></router-link>
   </ul>
 </template>
--- routing/src/components/user/User.vue
+++ routing/src/components/user/User.vue
@@ -2,6 +2,7 @@
     <div>
         <h1>The User Page</h1>
         <hr>
+        <p>Loaded ID: {{ id }}</p>
         <button @click="navigateToHome" class="btn btn-primary">Go To Home</button>
     </div>
 
@@ -9,6 +10,17 @@
 
 <script>
 export default {
+    data() {
+        return {
+            id: this.$route.params.id // accessing our params
+        }
+    },
+    // we have to watch the $params, because when it changes Vue wont recognise it has changed unless it is being watched
+    watch: {
+        '$route'(to, from) {
+            this.id = to.params.id;
+        }
+    },
     methods: {
         navigateToHome() {
             this.$router.push({path: '/'}); // programtically navigating
