c85eac0614eb24ca105b5be1e8f3fef3188908e1
Stephen Axtell <axtell.stephen@gmail.com>
2019-06-19 10:34:49 +0200

    adding child routes to User

--- routing/src/components/user/User.vue
+++ routing/src/components/user/User.vue
@@ -4,23 +4,25 @@
         <hr>
         <p>Loaded ID: {{ id }}</p>
         <button @click="navigateToHome" class="btn btn-primary">Go To Home</button>
+        <hr>
+        <router-view></router-view>
     </div>
 
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            id: this.$route.params.id // accessing our params
-        }
-    },
-    // we have to watch the $params, because when it changes Vue wont recognise it has changed unless it is being watched
-    watch: {
-        '$route'(to, from) {
-            this.id = to.params.id;
-        }
-    },
+    // data() {
+    //     return {
+    //         id: this.$route.params.id // accessing our params
+    //     }
+    // },
+    // // we have to watch the $params, because when it changes Vue wont recognise it has changed unless it is being watched
+    // watch: {
+    //     '$route'(to, from) {
+    //         this.id = to.params.id;
+    //     }
+    // },
     methods: {
         navigateToHome() {
             this.$router.push({path: '/'}); // programtically navigating
