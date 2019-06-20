3d1f211a6612f2b696685425cbc90ac8bae8d424
Stephen Axtell <axtell.stephen@gmail.com>
2019-06-20 18:17:49 +0200

    added some hash navigation

--- routing/src/main.js
+++ routing/src/main.js
@@ -9,7 +9,18 @@ Vue.use(VueRouter); // inserting new package into vue core
 // creating our new router
 const router = new VueRouter({
   routes: routes,
-  mode: 'history' // have to do this if you want nice clean urls, not the ones with a hash
+  mode: 'history', // have to do this if you want nice clean urls, not the ones with a hash
+  scrollBehavior(to, from, savedPosition) {
+
+    if (savedPosition) {
+      return savedPosition;
+    }
+
+    if (to.hash) {
+      return { selector: to.hash}
+    }
+    return {x: 0, y: 0 };
+  }
 });
 
 new Vue({
