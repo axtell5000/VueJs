ee68cef1fa4780ab8d143b7ba3f44e86db37fce0
Stephen Axtell <axtell.stephen@gmail.com>
2019-06-22 15:08:26 +0200

    added lazy loading for Users

--- routing/src/routes.js
+++ routing/src/routes.js
@@ -1,8 +1,31 @@
-import User from './components/user/User.vue';
 import Home from './components/Home.vue';
-import UserStart from './components/user/UserStart.vue';
-import UserDetail from './components/user/UserDetail.vue';
-import UserEdit from './components/user/UserEdit.vue';
+
+// Below is for lazy loading, loading code when you want it. This code below is mostly for webpack, hence the require
+const User = resolve => {
+  require.ensure(['./components/user/User.vue'], () => {
+    resolve(require('./components/user/User.vue'));
+  });
+};
+
+const UserStart = resolve => {
+  require.ensure(['./components/user/UserStart.vue'], () => {
+    resolve(require('./components/user/UserStart.vue'));
+  });
+};
+
+const UserEdit = resolve => {
+  require.ensure(['./components/user/UserEdit.vue'], () => {
+    resolve(require('./components/user/UserEdit.vue'));
+  });
+};
+
+const UserDetail = resolve => {
+  require.ensure(['./components/user/UserDetail.vue'], () => {
+    resolve(require('./components/user/UserDetail.vue'));
+  });
+};
+
+// Lazy loading code ends
 
 export const routes = [
   { path: '/', component: Home, name: 'home'},
